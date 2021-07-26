ed2bc6025cf659de01cdb71d3478a1249193452d
stevechow <zhmnda@icloud.com>
2021-07-26 15:32:18 +0800

    提交

---
+++ src/views/Test/test.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5];
+for (var key of arr) {
+  // 打印元素值
+  console.log(key);
+}
+
+let obj = {
+  name: "steve",
+  age: 20,
+};
+for (var key of obj) {
+  // 打印键
+  console.log(key);
+}
